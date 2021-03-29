1dcedaa2edce07c467cd693f21c71eb1114ed091
Daulet <anakhaev07@gmail.com>
2021-03-29 20:12:23 +0300

    work on validation

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from "vue";
 import Vueledate from "vuelidate";
 
+import "./assets/style/main.scss";
+
 import App from "./App.vue";
 Vue.use(Vueledate);
 Vue.config.productionTip = false;
+Vue.config.devtools = true;
 
 new Vue({
   render: (h) => h(App),
